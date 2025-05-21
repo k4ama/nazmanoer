a2cda9f0b9c07a50d61ee8d5ecf333398c38b731
k4ama <14azmanoer@gmail.com>
2025-05-21 14:01:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("forza ferrari 🏎️")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "this year is Our year"
 )
+st.image("
